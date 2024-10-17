7ad8e5aaa3dc5fd34bfed657796b9eb4928b82a6
Vladislav Ezhergin <vladyezh@gmail.com>
2024-10-17 21:05:46 +0000

    Tf changes

---
+++ diploma/step1/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.130.0"
+  hashes = [
+    "h1:icScUR3WtRFnxv8/nNGddryucfyT3PBj4nzh/KCsxBA=",
+  ]
+}
---
+++ diploma/step1/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+resource "yandex_iam_service_account" "sa" {
+  name        = "sa1"
+  description = "service account 1"
+}
